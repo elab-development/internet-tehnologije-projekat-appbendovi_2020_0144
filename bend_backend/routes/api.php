78776bf57141488f74d6e29e7ef8c741e41a81a3
Isidoraa2 <isidoraradisavljevic22@gmail.com>
2024-08-23 15:26:11 +0200

    Rute za nas API

--- bend_backend/routes/api.php
+++ bend_backend/routes/api.php
@@ -14,6 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('search', [\App\Http\Controllers\YouTubeController::class, 'search']);
+
+Route::apiResource('bands', \App\Http\Controllers\BandController::class)->only('index', 'show');
+Route::get('band-songs', [\App\Http\Controllers\BandSongController::class, 'index']);
+Route::get('band-songs/{id}', [\App\Http\Controllers\BandSongController::class, 'show']);
+Route::get('events', [\App\Http\Controllers\EventController::class, 'index']);
+Route::get('events/{id}', [\App\Http\Controllers\EventController::class, 'show']);
+
+Route::post('register', [\App\Http\Controllers\UserController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\UserController::class, 'login']);
+Route::get('paginacija', [\App\Http\Controllers\ChosenSongController::class, 'paginationSongs']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('band-songs', [\App\Http\Controllers\BandSongController::class, 'index']);
+    Route::apiResource('bands', \App\Http\Controllers\BandController::class)->only('store', 'update', 'destroy');
+    Route::post('band-songs', [\App\Http\Controllers\BandSongController::class, 'store']);
+    Route::put('band-songs/{id}', [\App\Http\Controllers\BandSongController::class, 'update']);
+    Route::delete('band-songs/{id}', [\App\Http\Controllers\BandSongController::class, 'destroy']);
+    Route::post('events', [\App\Http\Controllers\EventController::class, 'store']);
+    Route::delete('events/{id}', [\App\Http\Controllers\EventController::class, 'destroy']);
+
+    Route::post('logout', [\App\Http\Controllers\UserController::class, 'logout']);
+
+    Route::apiResource('chosen-songs', \App\Http\Controllers\ChosenSongController::class);
+    Route::get('chosen-songs-by-event/{eventId}', [\App\Http\Controllers\ChosenSongController::class, 'getChosenSongsByEvent']);
+    Route::get('favourite-bands', [\App\Http\Controllers\FavouriteBandController::class, 'index']);
+    Route::get('favourite-bands-by-user/{userId}', [\App\Http\Controllers\FavouriteBandController::class, 'findByUser']);
+    Route::get('favourite-bands-by-band/{bandId}', [\App\Http\Controllers\FavouriteBandController::class, 'findByBand']);
+    Route::post('favourite-bands', [\App\Http\Controllers\FavouriteBandController::class, 'store']);
+
 });
+
